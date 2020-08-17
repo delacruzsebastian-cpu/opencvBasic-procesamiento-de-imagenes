cecbaee75f91e5bd7fab51681b461e748b9079c4
delacruzsebastian-cpu <69725344+delacruzsebastian-cpu@users.noreply.github.com>
2020-08-17 16:16:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 from COLOR import *
+#SEBASTIAN DE LA CRUZ GUTIERREZ PUJ
 if __name__ == '__main__':
     route_image = input ('ingrese la direccion de la imagen: ')
     image = colorImage(route_image)
